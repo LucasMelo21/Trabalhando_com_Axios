f1ee4aafcf5179296c7e131bd275fca1374ef6cc
LucasMelo21 <lucasmelo.tec.saude@gmail.com>
2026-03-09 22:01:58 -0300

    Primeira request utilizando Axios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Teste");
+// 1 - Primeiro Request
+const getData = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
+};
+getData();
